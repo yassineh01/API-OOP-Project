8faca738ed225c82cadf55dadc1246e2361ebd5d
yassineh01 <132911589+yassineh01@users.noreply.github.com>
2024-08-27 13:00:20 +0200

    Add files via upload

---
+++ TestFindPlace/Program.cs
@@ -0,0 +1,13 @@
+﻿using Plaats;
+
+Console.WriteLine("which place are you looking for?");
+var searchPlace = Console.ReadLine();
+
+if (!string.IsNullOrWhiteSpace(searchPlace))
+{
+    var placeDatabase = new PlaceDatabase("geo");
+    foreach (var place in placeDatabase.Search(searchPlace))
+    {
+        Console.WriteLine(place.Naam_2);
+    }
+}   
